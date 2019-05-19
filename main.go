3b512f1b457c8f35e6c0c9683ffcadd243496e82
esrrhs <esrrhs@163.com>
2019-05-19 16:10:37 +0800

    ａｄｄ

--- main.go
+++ main.go
@@ -23,10 +23,10 @@ func main() {
 	loggo.Info("%v", texas.BytesToStr(d))
 
 	spider.Load()
-	config := spider.Config{1, 100, 1000, 2, true}
+	config := spider.Config{1, 100, 1000, 4, true}
 	//entry := []string{"https://ygdy8.com"}
 	//entry := []string{"http://www.csdn.net"}
-	entry := []string{"https://www.80ying.com/actor/17630"}
+	entry := []string{"https://www.80ying.com"}
 	spider.Start(config, entry)
 
 	texas.Load()
