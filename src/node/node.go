1caab51afa91abb2855c7754acd2e546774e2d24
esrrhs <esrrhs@163.com>
2019-06-08 19:16:40 +0800

    ａｄｄ

---
+++ src/node/node.go
@@ -0,0 +1,71 @@
+package node
+
+import (
+	"context"
+	"fmt"
+	"loggo"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var gNodeDir string
+
+func Ini() {
+
+	GOPATH := os.Getenv("GOPATH")
+	loggo.Info("GOPATH %v", GOPATH)
+
+	gpaths := strings.Split(GOPATH, ";")
+
+	dir := ""
+	for _, p := range gpaths {
+		tmp := p + "/node/"
+		tmp = filepath.Clean(tmp)
+		tmp = filepath.ToSlash(tmp)
+		if _, err := os.Stat(tmp); !os.IsNotExist(err) {
+			dir = tmp
+			break
+		}
+	}
+
+	if len(dir) <= 0 {
+		return
+	}
+	loggo.Info("dir %v", dir)
+
+	gNodeDir = dir
+}
+
+func Run(script string, timeout int, param ...string) string {
+	d := time.Now().Add(time.Duration(timeout) * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+
+	defer cancel() // releases resources if slowOperation completes before timeout elapses
+
+	loggo.Info("node Run start %v %v ", script, fmt.Sprint(param))
+
+	tmp := gNodeDir + "/" + script
+	tmp = filepath.Clean(tmp)
+	tmp = filepath.ToSlash(tmp)
+
+	var tmpparam []string
+	tmpparam = append(tmpparam, tmp)
+	tmpparam = append(tmpparam, param...)
+
+	begin := time.Now()
+	cmd := exec.CommandContext(ctx, "node", tmpparam...)
+	out, err := cmd.CombinedOutput()
+	outstr := string(out)
+	if err != nil {
+		loggo.Warn("node Run fail %v %v %v", cmd.Args, outstr, ctx.Err())
+		return ""
+	}
+
+	loggo.Info("node Run ok %v %v", cmd.Args, time.Now().Sub(begin))
+	loggo.Info("%v", outstr)
+
+	return outstr
+}
