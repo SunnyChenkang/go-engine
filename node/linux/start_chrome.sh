4deeca121c676df5006c8d8978973e054b2fb512
esrrhs <esrrhs@163.com>
2019-06-09 16:23:01 +0800

    add

--- node/linux/start_chrome.sh
+++ node/linux/start_chrome.sh
@@ -1,10 +1,3 @@
 #! /bin/sh
 cd $1
-NUM=`ps -ef | grep chrome | grep chrome-linux | grep -v grep | wc -l`
-if [ $NUM -gt 0 ]
-then
-       exit 0
-else
-       cd $1
-       node start_chrome.js &
-fi
+node start_chrome.js > chrome.pid
